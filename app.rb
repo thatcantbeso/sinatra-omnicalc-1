c1b69e920c1a14451c05f37068e5f955b255fc2c
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-09-19 23:41:22 +0000

    halfway done

--- app.rb
+++ app.rb
@@ -7,3 +7,33 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/bison")do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+    erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = Math.sqrt(@the_num).to_f
+  erb(:square_root_results)
+end
